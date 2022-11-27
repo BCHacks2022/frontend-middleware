738c874eead338f2fd086f5c85ddf47c96f38f39
Murad Shahmammadli <64356412+MuradSh@users.noreply.github.com>
2022-11-26 16:55:22 -0800

    WishList

--- lib/models/company.tsx
+++ lib/models/company.tsx
@@ -3,11 +3,13 @@ export class company {
     name: String;
     link: String;
     elId: String;
+    score: String;
 
-    constructor(image: String, name: String, link: String, elId: String) {
+    constructor(image: String, name: String, link: String, elId: String, score: String) {
         this.image = image;
         this.name = name;
         this.link = link;
         this.elId = elId;
+        this.score = score;
     }
 }
